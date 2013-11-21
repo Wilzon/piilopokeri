3fb3e72109ca2683dfed1007fa818e96dfa53e10
Otto Wigren <otto.wigren@gmail.com>
2013-11-21 21:54:23 +0200

    lisätty J-Unit

--- Piilopokeri/test/piilopokeri/domain/Piilopokeri2Test.java
+++ Piilopokeri/test/piilopokeri/domain/Piilopokeri2Test.java
@@ -12,120 +12,202 @@ import piilopokeri.domain.Kortti;
 public class Piilopokeri2Test {
     Piilopokeri pokeri;
     
-    Kasi viitosKasi;
-    Kasi nelosKasi;
-    Kasi kolmosKasi;
-    Kasi kaksiPariaKasi;
-    Kasi pariKasi;
-    Kasi suoraKasi;
-    Kasi variKasi;
-    Kasi varisuoraKasi;
-    Kasi tayskasiKasi;
+    Kasi huonoViitosKasi;
+    Kasi hyvaViitosKasi;
+    
+    Kasi huonoNelosKasi;
+    Kasi hyvaNelosKasi;
+    
+    Kasi huonoKolmosKasi;
+    Kasi hyvaKolmosKasi;
+    
+    Kasi huonoKaksiPariaKasi;
+    Kasi hyvaKaksiPariaKasi;
+    
+    Kasi huonoPariKasi;
+    Kasi hyvaPariKasi;
+    
+    Kasi huonoSuoraKasi;
+    Kasi hyvaSuoraKasi;
+    
+    Kasi huonoVariKasi;
+    Kasi hyvaVariKasi;
+    
+    Kasi huonoVarisuoraKasi;
+    Kasi hyvaVarisuoraKasi;
+    
+    Kasi huonoTayskasiKasi;
+    Kasi hyvaTayskasiKasi;
+    
     Kasi huonoEiMitaanKasi;
     Kasi hyvaEiMitaanKasi;
-    
-    Kortti pata5;
-    Kortti pata6;
-    Kortti pata7;
-    Kortti pata8;
-    Kortti pata9;
-    
-    Kortti pata3;
-    Kortti ruutu8;
-    Kortti herttaKuningas;
-    
-    Kortti ruutu9;
-    Kortti risti9;
-    Kortti hertta9;
-    
-    Kortti jokeri;
-    
     
     @Before
     public void setUp() {
         pokeri = new Piilopokeri();
         
-        viitosKasi = new Kasi();
-        nelosKasi = new Kasi();
-        kolmosKasi = new Kasi();
-        kaksiPariaKasi = new Kasi();
-        pariKasi = new Kasi();
-        suoraKasi = new Kasi();
-        variKasi = new Kasi();
-        varisuoraKasi = new Kasi();
-        tayskasiKasi = new Kasi();
+        huonoViitosKasi = new Kasi();
+        hyvaViitosKasi = new Kasi();
+        
+        huonoNelosKasi = new Kasi();
+        hyvaNelosKasi = new Kasi();
+        
+        huonoKolmosKasi = new Kasi();
+        hyvaKolmosKasi = new Kasi();
+        
+        huonoKaksiPariaKasi = new Kasi();
+        hyvaKaksiPariaKasi = new Kasi();
+        
+        huonoPariKasi = new Kasi();
+        hyvaPariKasi = new Kasi();
+        
+        
+        huonoSuoraKasi = new Kasi();
+        hyvaSuoraKasi = new Kasi();
+        
+        huonoVariKasi = new Kasi();
+        hyvaVariKasi = new Kasi();
+        
+        huonoVarisuoraKasi = new Kasi();
+        hyvaVarisuoraKasi = new Kasi();
+        
+        huonoTayskasiKasi = new Kasi();
+        hyvaTayskasiKasi = new Kasi();
+        
         huonoEiMitaanKasi = new Kasi();
         hyvaEiMitaanKasi = new Kasi();
         
-        pata5 = new Kortti(5, Kortti.PATA, true);
-        pata6 = new Kortti(6, Kortti.PATA, true);
-        pata7 = new Kortti(7, Kortti.PATA, true);
-        pata8 = new Kortti(8, Kortti.PATA, true);
-        pata9 = new Kortti(9, Kortti.PATA, true);
+        Kortti pata5 = new Kortti(5, Kortti.PATA, true);
+        Kortti pata6 = new Kortti(6, Kortti.PATA, true);
+        Kortti pata7 = new Kortti(7, Kortti.PATA, true);
+        Kortti pata8 = new Kortti(8, Kortti.PATA, true);
+        Kortti pata9 = new Kortti(9, Kortti.PATA, true);
         
-        pata3 = new Kortti(3, Kortti.PATA, true);
-        ruutu8 = new Kortti(8, Kortti.RUUTU, true);
-        herttaKuningas = new Kortti(13, Kortti.HERTTA, true);
+        Kortti pata3 = new Kortti(3, Kortti.PATA, true);
+        Kortti ruutu8 = new Kortti(8, Kortti.RUUTU, true);
         
-        risti9 = new Kortti(9, Kortti.RISTI, true);
-        ruutu9 = new Kortti(9, Kortti.RUUTU, true);
-        hertta9 = new Kortti(9, Kortti.HERTTA, true);
+        Kortti herttaK = new Kortti(13, Kortti.HERTTA, true);
+        Kortti pataK = new Kortti(13, Kortti.PATA, true);
+        Kortti ruutuK = new Kortti(13, Kortti.RUUTU, true);
+        Kortti ristiK = new Kortti(13, Kortti.RISTI, true);
         
-        jokeri = new Kortti(15, Kortti.JOKERI, true);
+        Kortti pata10 = new Kortti(10, Kortti.PATA, true);
+        Kortti pataJ = new Kortti(10, Kortti.PATA, true);
+        Kortti pataQ = new Kortti(10, Kortti.PATA, true);
         
-        viitosKasi.lisaaKortti(hertta9);
-        viitosKasi.lisaaKortti(risti9);
-        viitosKasi.lisaaKortti(ruutu9);
-        viitosKasi.lisaaKortti(pata9);
-        viitosKasi.lisaaKortti(jokeri);
+        Kortti risti9 = new Kortti(9, Kortti.RISTI, true);
+        Kortti ruutu9 = new Kortti(9, Kortti.RUUTU, true);
+        Kortti hertta9 = new Kortti(9, Kortti.HERTTA, true);
         
-        nelosKasi.lisaaKortti(hertta9);
-        nelosKasi.lisaaKortti(risti9);
-        nelosKasi.lisaaKortti(ruutu9);
-        nelosKasi.lisaaKortti(pata9);
-        nelosKasi.lisaaKortti(pata5);
+        Kortti jokeri = new Kortti(15, Kortti.JOKERI, true);
         
-        kolmosKasi.lisaaKortti(hertta9);
-        kolmosKasi.lisaaKortti(risti9);
-        kolmosKasi.lisaaKortti(ruutu9);
-        kolmosKasi.lisaaKortti(pata5);
-        kolmosKasi.lisaaKortti(pata6);
+        huonoViitosKasi.lisaaKortti(hertta9);
+        huonoViitosKasi.lisaaKortti(risti9);
+        huonoViitosKasi.lisaaKortti(ruutu9);
+        huonoViitosKasi.lisaaKortti(pata9);
+        huonoViitosKasi.lisaaKortti(jokeri);
         
-        kaksiPariaKasi.lisaaKortti(hertta9);
-        kaksiPariaKasi.lisaaKortti(risti9);
-        kaksiPariaKasi.lisaaKortti(pata8);
-        kaksiPariaKasi.lisaaKortti(ruutu8);
-        kaksiPariaKasi.lisaaKortti(pata5);
+        hyvaViitosKasi.lisaaKortti(herttaK);
+        hyvaViitosKasi.lisaaKortti(pataK);
+        hyvaViitosKasi.lisaaKortti(jokeri);
+        hyvaViitosKasi.lisaaKortti(ristiK);
+        hyvaViitosKasi.lisaaKortti(ruutuK);
         
-        pariKasi.lisaaKortti(hertta9);
-        pariKasi.lisaaKortti(pata9);
-        pariKasi.lisaaKortti(pata7);
-        pariKasi.lisaaKortti(pata6);
-        pariKasi.lisaaKortti(pata5);
+        huonoNelosKasi.lisaaKortti(hertta9);
+        huonoNelosKasi.lisaaKortti(risti9);
+        huonoNelosKasi.lisaaKortti(ruutu9);
+        huonoNelosKasi.lisaaKortti(pata9);
+        huonoNelosKasi.lisaaKortti(pata5);
         
-        suoraKasi.lisaaKortti(pata5);
-        suoraKasi.lisaaKortti(hertta9);
-        suoraKasi.lisaaKortti(pata7);
-        suoraKasi.lisaaKortti(pata8);
-        suoraKasi.lisaaKortti(pata6);
+        hyvaNelosKasi.lisaaKortti(pataK);
+        hyvaNelosKasi.lisaaKortti(ristiK);
+        hyvaNelosKasi.lisaaKortti(pataJ);
+        hyvaNelosKasi.lisaaKortti(herttaK);
+        hyvaNelosKasi.lisaaKortti(ruutuK);
         
-        variKasi.lisaaKortti(pata9);
-        variKasi.lisaaKortti(pata8);
-        variKasi.lisaaKortti(pata7);
-        variKasi.lisaaKortti(pata6);
-        variKasi.lisaaKortti(pata3);
+        huonoKolmosKasi.lisaaKortti(hertta9);
+        huonoKolmosKasi.lisaaKortti(risti9);
+        huonoKolmosKasi.lisaaKortti(ruutu9);
+        huonoKolmosKasi.lisaaKortti(pata5);
+        huonoKolmosKasi.lisaaKortti(pata6);
         
-        varisuoraKasi.lisaaKortti(pata5);
-        varisuoraKasi.lisaaKortti(pata6);
-        varisuoraKasi.lisaaKortti(pata7);
-        varisuoraKasi.lisaaKortti(pata8);
-        varisuoraKasi.lisaaKortti(pata9);
+        hyvaKolmosKasi.lisaaKortti(pata8);
+        hyvaKolmosKasi.lisaaKortti(hertta9);
+        hyvaKolmosKasi.lisaaKortti(pataK);
+        hyvaKolmosKasi.lisaaKortti(herttaK);
+        hyvaKolmosKasi.lisaaKortti(ruutuK);
         
-        tayskasiKasi.lisaaKortti(hertta9);
-        tayskasiKasi.lisaaKortti(pata9);
-        tayskasiKasi.lisaaKortti(ruutu9);
-        tayskasiKasi.lisaaKortti(ruutu8);
-        tayskasiKasi.lisaaKortti(pata8);
+        huonoKaksiPariaKasi.lisaaKortti(hertta9);
+        huonoKaksiPariaKasi.lisaaKortti(risti9);
+        huonoKaksiPariaKasi.lisaaKortti(pata8);
+        huonoKaksiPariaKasi.lisaaKortti(ruutu8);
+        huonoKaksiPariaKasi.lisaaKortti(pata5);
+        
+        hyvaKaksiPariaKasi.lisaaKortti(pataK);
+        hyvaKaksiPariaKasi.lisaaKortti(herttaK);
+        hyvaKaksiPariaKasi.lisaaKortti(pata5);
+        hyvaKaksiPariaKasi.lisaaKortti(ruutu9);
+        hyvaKaksiPariaKasi.lisaaKortti(pata9);
+        
+        huonoPariKasi.lisaaKortti(hertta9);
+        huonoPariKasi.lisaaKortti(pata9);
+        huonoPariKasi.lisaaKortti(pata7);
+        huonoPariKasi.lisaaKortti(pata6);
+        huonoPariKasi.lisaaKortti(pata5);
+        
+        hyvaPariKasi.lisaaKortti(pataK);
+        hyvaPariKasi.lisaaKortti(pata3);
+        hyvaPariKasi.lisaaKortti(pata7);
+        hyvaPariKasi.lisaaKortti(herttaK);
+        hyvaPariKasi.lisaaKortti(hertta9);
+
+        huonoSuoraKasi.lisaaKortti(pata5);
+        huonoSuoraKasi.lisaaKortti(hertta9);
+        huonoSuoraKasi.lisaaKortti(pata7);
+        huonoSuoraKasi.lisaaKortti(pata8);
+        huonoSuoraKasi.lisaaKortti(pata6);
+        
+        hyvaSuoraKasi.lisaaKortti(pataK);
+        hyvaSuoraKasi.lisaaKortti(pataQ);
+        hyvaSuoraKasi.lisaaKortti(hertta9);
+        hyvaSuoraKasi.lisaaKortti(pata10);
+        hyvaSuoraKasi.lisaaKortti(pataJ);
+        
+        huonoVariKasi.lisaaKortti(pata9);
+        huonoVariKasi.lisaaKortti(pata8);
+        huonoVariKasi.lisaaKortti(pata7);
+        huonoVariKasi.lisaaKortti(pata6);
+        huonoVariKasi.lisaaKortti(pata3);
+        
+        hyvaVariKasi.lisaaKortti(pata9);
+        hyvaVariKasi.lisaaKortti(pata8);
+        hyvaVariKasi.lisaaKortti(pata7);
+        hyvaVariKasi.lisaaKortti(pata3);
+        hyvaVariKasi.lisaaKortti(pataK);
+        
+        huonoVarisuoraKasi.lisaaKortti(pata5);
+        huonoVarisuoraKasi.lisaaKortti(pata6);
+        huonoVarisuoraKasi.lisaaKortti(pata7);
+        huonoVarisuoraKasi.lisaaKortti(pata8);
+        huonoVarisuoraKasi.lisaaKortti(pata9);
+        
+        hyvaVarisuoraKasi.lisaaKortti(pataK);
+        hyvaVarisuoraKasi.lisaaKortti(pataJ);
+        hyvaVarisuoraKasi.lisaaKortti(pata10);
+        hyvaVarisuoraKasi.lisaaKortti(pataQ);
+        hyvaVarisuoraKasi.lisaaKortti(pata9);
+        
+        huonoTayskasiKasi.lisaaKortti(hertta9);
+        huonoTayskasiKasi.lisaaKortti(pata9);
+        huonoTayskasiKasi.lisaaKortti(ruutu9);
+        huonoTayskasiKasi.lisaaKortti(ruutu8);
+        huonoTayskasiKasi.lisaaKortti(pata8);
+        
+        hyvaTayskasiKasi.lisaaKortti(pataK);
+        hyvaTayskasiKasi.lisaaKortti(herttaK);
+        hyvaTayskasiKasi.lisaaKortti(ruutuK);
+        hyvaTayskasiKasi.lisaaKortti(risti9);
+        hyvaTayskasiKasi.lisaaKortti(pata9);
         
         huonoEiMitaanKasi.lisaaKortti(pata5);
         huonoEiMitaanKasi.lisaaKortti(pata6);
@@ -137,53 +219,53 @@ public class Piilopokeri2Test {
         hyvaEiMitaanKasi.lisaaKortti(pata6);
         hyvaEiMitaanKasi.lisaaKortti(pata7);
         hyvaEiMitaanKasi.lisaaKortti(pata8);
-        hyvaEiMitaanKasi.lisaaKortti(herttaKuningas);
+        hyvaEiMitaanKasi.lisaaKortti(herttaK);
         
     }
-    
+
     @Test
     public void viitosetVoittaaVarisuoran() {
-        assertEquals(1, pokeri.parempiKasi(viitosKasi, varisuoraKasi));
+        assertEquals(1, pokeri.parempiKasi(huonoViitosKasi, huonoVarisuoraKasi));
     }
     
     @Test
     public void varisuoraVoittaaNeloset() {
-        assertEquals(1, pokeri.parempiKasi(varisuoraKasi, nelosKasi));
+        assertEquals(1, pokeri.parempiKasi(huonoVarisuoraKasi, huonoNelosKasi));
     }
     
     @Test
     public void nelosetVoittavatTayskaden() {
-        assertEquals(1, pokeri.parempiKasi(nelosKasi, tayskasiKasi));
+        assertEquals(1, pokeri.parempiKasi(huonoNelosKasi, huonoTayskasiKasi));
     }
     
     @Test
     public void tayskasiVoittaaVarin() {
-        assertEquals(1, pokeri.parempiKasi(tayskasiKasi, variKasi));
+        assertEquals(1, pokeri.parempiKasi(huonoTayskasiKasi, huonoVariKasi));
     }
     
     @Test
     public void variVoittaaSuoran() {
-        assertEquals(1, pokeri.parempiKasi(variKasi, suoraKasi));
+        assertEquals(1, pokeri.parempiKasi(huonoVariKasi, huonoSuoraKasi));
     }
     
     @Test
     public void suoraVoittaaKolmoset() {
-        assertEquals(1, pokeri.parempiKasi(suoraKasi, kolmosKasi));
+        assertEquals(1, pokeri.parempiKasi(huonoSuoraKasi, huonoKolmosKasi));
     }
     
     @Test
     public void kolmosetVoittaaKaksiParia() {
-        assertEquals(1, pokeri.parempiKasi(kolmosKasi, kaksiPariaKasi));
+        assertEquals(1, pokeri.parempiKasi(huonoKolmosKasi, huonoKaksiPariaKasi));
     }
     
     @Test
     public void kaksiPariaVoittaaParin() {
-        assertEquals(1, pokeri.parempiKasi(kaksiPariaKasi, pariKasi));
+        assertEquals(1, pokeri.parempiKasi(huonoKaksiPariaKasi, huonoPariKasi));
     }
     
     @Test
     public void pariVoittaaEiMitaanKaden() {
-        assertEquals(1, pokeri.parempiKasi(pariKasi, hyvaEiMitaanKasi));
+        assertEquals(1, pokeri.parempiKasi(huonoPariKasi, hyvaEiMitaanKasi));
     }
     
     @Test
@@ -191,4 +273,48 @@ public class Piilopokeri2Test {
         assertEquals(1, pokeri.parempiKasi(hyvaEiMitaanKasi, huonoEiMitaanKasi));
     }
     
+    @Test
+    public void pienempiKorttiHaviaa() {
+        assertEquals(-1, pokeri.parempiKasi(huonoEiMitaanKasi, hyvaEiMitaanKasi));
+    }
+    
+    @Test
+    public void parempiPariVoittaaHuonommanParin() {
+        assertEquals(1, pokeri.parempiKasi(hyvaPariKasi, huonoPariKasi));
+    }
+    
+    @Test
+    public void paremmatKolmosetVoittaaHuonommatKolmoset() {
+        assertEquals(1, pokeri.parempiMontaSamaa(hyvaKolmosKasi, huonoKolmosKasi, 3));
+    }
+    
+    @Test
+    public void paremmatNelosetVoittaaHuonommatNeloset() {
+        assertEquals(1, pokeri.parempiMontaSamaa(hyvaNelosKasi, huonoNelosKasi, 4));
+    }
+    
+    @Test
+    public void paremmatVitosetVoittaaHuonommatVitoset() {
+        assertEquals(1, pokeri.parempiMontaSamaa(hyvaViitosKasi, huonoViitosKasi, 5));
+    }
+    
+    @Test
+    public void parempiSuoraVoittaaHuonommanSuoran() {
+        assertEquals(1, pokeri.parempiSuoraTaiVari(hyvaSuoraKasi, huonoSuoraKasi));
+    }
+    
+    @Test
+    public void parempiVariVoittaaHuonommanVarin() {
+        assertEquals(1, pokeri.parempiSuoraTaiVari(hyvaVariKasi, huonoVariKasi));
+    }
+    
+    @Test
+    public void parempiTayskasiVoittaaHuonommanTayskaden() {
+        assertEquals(1, pokeri.parempiTayskasi(hyvaTayskasiKasi, huonoTayskasiKasi));
+    }
+    
+    @Test
+    public void parempiVarisuoraVoittaaHuonommanVarisuoran() {
+        assertEquals(1, pokeri.parempiSuoraTaiVari(hyvaVarisuoraKasi, huonoVarisuoraKasi));
+    }
 }
